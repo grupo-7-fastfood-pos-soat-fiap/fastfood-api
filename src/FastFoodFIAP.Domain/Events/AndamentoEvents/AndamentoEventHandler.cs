c8fe304915a390a994476a9d959c49f86b85d627
Luis Fernando <luisfernandodass@gmail.com>
2024-03-19 18:39:42 -0300

    fix: ajuste duplicação de código

--- src/FastFoodFIAP.Domain/Events/AndamentoEvents/AndamentoEventHandler.cs
+++ src/FastFoodFIAP.Domain/Events/AndamentoEvents/AndamentoEventHandler.cs
@@ -8,48 +8,34 @@ namespace FastFoodFIAP.Domain.Events.AndamentoEvents
     {
         AmazonSQSClient amazonSQSClient = new AmazonSQSClient();
 
-
         public async Task Handle(AndamentoCreateEvent notification)
         {
             Andamento andamento = new Andamento(notification.PedidoId, notification.FuncionarioId, notification.SituacaoId);
 
             if (andamento.SituacaoId == 2)
             {
-                var request = new SendMessageRequest();
-                request.QueueUrl = "https://sqs.us-east-1.amazonaws.com/381491906285/PedidoPreparacao.fifo";
-                request.MessageBody = andamento.PedidoId.ToString();
-                request.MessageGroupId = Guid.NewGuid().ToString();
-                request.MessageDeduplicationId = Guid.NewGuid().ToString();
-
-                // em preparacao
-                await amazonSQSClient.SendMessageAsync(request);
-            }
-            
-            if (andamento.SituacaoId == 3)
+                await PublicarMensagemNaFila(andamento, "https://sqs.us-east-1.amazonaws.com/381491906285/PedidoPreparacao.fifo");
+            } else if (andamento.SituacaoId == 3)
             {
-                var request = new SendMessageRequest();
-                request.QueueUrl = "https://sqs.us-east-1.amazonaws.com/381491906285/PedidoPronto.fifo";
-                request.MessageBody = andamento.PedidoId.ToString();
-                request.MessageGroupId = Guid.NewGuid().ToString();
-                request.MessageDeduplicationId = Guid.NewGuid().ToString();
-
-                // pronto
-                await amazonSQSClient.SendMessageAsync(request);
-            }
-            
-            if (andamento.SituacaoId == 4)
+                await PublicarMensagemNaFila(andamento, "https://sqs.us-east-1.amazonaws.com/381491906285/PedidoPronto.fifo");
+            } else if (andamento.SituacaoId == 4)
             {
-                var request = new SendMessageRequest();
-                request.QueueUrl = "https://sqs.us-east-1.amazonaws.com/381491906285/PedidoFinalizado.fifo";
-                request.MessageBody = andamento.PedidoId.ToString();
-                request.MessageGroupId = Guid.NewGuid().ToString();
-                request.MessageDeduplicationId = Guid.NewGuid().ToString();
-                
-                // postar na fila de finalizado.
-                await amazonSQSClient.SendMessageAsync(request);
+                await PublicarMensagemNaFila(andamento, "https://sqs.us-east-1.amazonaws.com/381491906285/PedidoFinalizado.fifo");
             }
 
             return;            
-        }       
+        }
+
+        public async Task PublicarMensagemNaFila(Andamento andamento, string queueURL)
+        {
+            var request = new SendMessageRequest();
+            request.QueueUrl = queueURL;
+            request.MessageBody = andamento.PedidoId.ToString();
+            request.MessageGroupId = Guid.NewGuid().ToString();
+            request.MessageDeduplicationId = Guid.NewGuid().ToString();
+
+            // em preparacao
+            await amazonSQSClient.SendMessageAsync(request);
+        }
     }
 }
